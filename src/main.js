3ad5c40c46827e4779713ba9627fb9ef44dce92c
jiayingxuu <18164012171@163.com>
2019-02-12 21:25:14 +0800

    day01

--- src/main.js
+++ src/main.js
@@ -4,9 +4,15 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import vueResource from 'vue-resource'
+Vue.use(vueResource)
+
 //导入mui
 import './libs/mui/css/mui.css'
 import './libs/mui/css/icons-extra.css'
+
+//初始化样式
+import './styles/common.css'
 
 import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.css'
