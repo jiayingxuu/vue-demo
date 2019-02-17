a633b46568e5ad36d2c0275cd489d08c70cf4ff7
jiayingxuu <18164012171@163.com>
2019-02-17 15:53:38 +0800

    加入购物车

--- src/pages/goodsdesc/index.vue
+++ src/pages/goodsdesc/index.vue
@@ -1,5 +1,46 @@
 <template>
-    <div>
-        11111111111
-    </div>
+  <div class="goodsdesc_container">
+    <h1>{{content.title}}</h1>
+    <hr>
+    <div class="text" v-html="content.content"></div>
+  </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      content: {}
+    };
+  },
+  created() {
+    this.getcontent();
+  },
+  methods: {
+    getcontent() {
+      this.$http.get("goods/getdesc/" + this.$route.params.id).then(res => {
+        this.content = res.body.message[0];
+      });
+    }
+  }
+};
+</script>
+
+<style lang="less">
+.goodsdesc_container {
+  padding: 5px;
+  h1 {
+    font-size: 16px;
+    font-weight: 700px;
+    color: #226aff;
+    text-align: center;
+    margin: 15px 0;
+  }
+  .text {
+      font-size: 13px;
+      line-height: 18px;
+    img {
+      width: 100%;
+    }
+  }
+}
+</style>
--- src/pages/goodsinfo/index.vue
+++ src/pages/goodsinfo/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="goodsinfo_container">
+    <!-- 小球 -->
+    <transition @before-enter="beforeEnter" @enter="enter" @after-enter="afterEnter">
+      <div class="ball" v-show="ballFall" ref="ball"></div>
+    </transition>
     <!-- 轮播图 -->
     <div class="mui-card">
       <div class="mui-card-content">
@@ -31,7 +35,7 @@
           </div>
           <p>
             <mt-button type="primary" size="small">立即购买</mt-button>
-            <mt-button type="danger" size="small">加入购物车</mt-button>
+            <mt-button type="danger" size="small" @click="addShopcar">加入购物车</mt-button>
           </p>
         </div>
       </div>
@@ -40,7 +44,7 @@
     <div class="mui-card">
       <div class="mui-card-header">商品参数</div>
       <div class="mui-card-content">
-        <div class="mui-card-content-inner">
+        <div class="mui-card-content-inner goodstext">
           <p>商品库号: {{goodsinfo.goods_no}}</p>
           <p>库存情况: {{goodsinfo.stock_quantity}} 件</p>
           <p>上架时间: {{goodsinfo.add_time | dateformet}}</p>
@@ -61,7 +65,8 @@ export default {
       id: this.$route.params.id,
       banner: [],
       goodsinfo: {},
-      buyCount: 1
+      buyCount: 1,
+      ballFall:false
     };
   },
   created() {
@@ -77,7 +82,6 @@ export default {
     getGoodsinfo() {
       this.$http.get("goods/getinfo/" + this.id).then(res => {
         this.goodsinfo = res.body.message[0];
-        console.log(this.goodsinfo);
       });
     },
     getbigestCount() {
@@ -90,6 +94,29 @@ export default {
     },
     getComment(id){
         this.$router.push('/home/goodscomment' + id)
+    },
+    addShopcar(){
+      this.ballFall = !this.ballFall 
+    },
+    beforeEnter(el){
+      el.style.transform = 'translate(0,0)'
+    },
+    enter(el,done){
+      // el.offsetWidth;
+
+      const ballPostion = this.$refs.ball.getBoundingClientRect();
+      
+      const badgePostion = document.getElementById('badge').getBoundingClientRect();
+
+      const distX = badgePostion.left - ballPostion.left;
+      const distY = badgePostion.top - ballPostion.top;
+
+      el.style.transform = `translate(${distX}px,${distY}px)`;
+      el.style.transition = "all 0.5s cubic-bezier(.4,-0.3,1,.68)";
+      done()
+    },
+    afterEnter(){
+      this.ballFall = !this.ballFall
     }
   }
 };
@@ -133,4 +160,20 @@ export default {
       margin: 15px 0;
     }
   }
+.goodstext {
+  p {
+    font-size: 14px;
+    line-height: 20px;
+  }
+}
+.ball {
+  z-index: 99;
+  width: 15px;
+  height: 15px;
+  background-color: red;
+  border-radius: 50%;
+  position: absolute;
+  top: 392px;
+  left: 120px;
+}
 </style>
